78a6ae2dae538e8270a16f716e6c317a7cbe8051
David Li <davecyli@gmail.com>
2025-03-01 03:44:44 -0800

    saving work. TODO: include eigen library into build manifest

--- OpticalFlow/driver.cpp
+++ OpticalFlow/driver.cpp
@@ -9,10 +9,99 @@ Changes to make prior to GPU implementation
 #include "spatialConvolution.h"
 #include "temporalConvolution.h"
 
+#include <Eigen>
+
 #include <opencv2/opencv.hpp>
+
+#include <rerun.hpp>
+#include <rerun/demo_utils.hpp>
 
 using namespace std;
 using namespace cv;
+using namespace rerun::demo;
+
+
+template <>
+struct rerun::CollectionAdapter<uint8_t, cv::Mat>
+{
+    /* Adapters to borrow an OpenCV image into Rerun
+     * images without copying */
+
+    Collection<uint8_t> operator()(const cv::Mat& img)
+    {
+        // Borrow for non-temporary.
+
+        assert("OpenCV matrix expected have bit depth CV_U8" && CV_MAT_DEPTH(img.type()) == CV_8U);
+        return Collection<uint8_t>::borrow(img.data, img.total() * img.channels());
+    }
+
+    Collection<uint8_t> operator()(cv::Mat&& img)
+    {
+        /* Do a full copy for temporaries (otherwise the data
+         * might be deleted when the temporary is destroyed). */
+
+        assert("OpenCV matrix expected have bit depth CV_U8" && CV_MAT_DEPTH(img.type()) == CV_8U);
+        std::vector<uint8_t> img_vec(img.total() * img.channels());
+        img_vec.assign(img.data, img.data + img.total() * img.channels());
+        return Collection<uint8_t>::take_ownership(std::move(img_vec));
+    }
+};
+
+
+template <>
+struct rerun::CollectionAdapter<rerun::Position3D, std::vector<Eigen::Vector3f>>
+{
+    /* Adapters to log eigen vectors as rerun positions*/
+
+    Collection<rerun::Position3D> operator()(const std::vector<Eigen::Vector3f>& container)
+    {
+        // Borrow for non-temporary.
+        return Collection<rerun::Position3D>::borrow(container.data(), container.size());
+    }
+
+    Collection<rerun::Position3D> operator()(std::vector<Eigen::Vector3f>&& container)
+    {
+        /* Do a full copy for temporaries (otherwise the data
+         * might be deleted when the temporary is destroyed). */
+        std::vector<rerun::Position3D> positions(container.size());
+        memcpy(positions.data(), container.data(), container.size() * sizeof(Eigen::Vector3f));
+        return Collection<rerun::Position3D>::take_ownership(std::move(positions));
+    }
+};
+
+
+template <>
+struct rerun::CollectionAdapter<rerun::Position3D, Eigen::Matrix3Xf>
+{
+    /* Adapters so we can log an eigen matrix as rerun positions */
+
+    // Sanity check that this is binary compatible.
+    static_assert(
+        sizeof(rerun::Position3D) == sizeof(Eigen::Matrix3Xf::Scalar) * Eigen::Matrix3Xf::RowsAtCompileTime
+        );
+
+    Collection<rerun::Position3D> operator()(const Eigen::Matrix3Xf& matrix)
+    {
+        // Borrow for non-temporary.
+        static_assert(alignof(rerun::Position3D) <= alignof(Eigen::Matrix3Xf::Scalar));
+        return Collection<rerun::Position3D>::borrow(
+            // Cast to void because otherwise Rerun will try to do above sanity checks with the wrong type (scalar).
+            reinterpret_cast<const void*>(matrix.data()),
+            matrix.cols()
+        );
+    }
+
+    Collection<rerun::Position3D> operator()(Eigen::Matrix3Xf&& matrix)
+    {
+        /* Do a full copy for temporaries (otherwise the
+         * data might be deleted when the temporary is destroyed). */
+        std::vector<rerun::Position3D> positions(matrix.cols());
+        memcpy(positions.data(), matrix.data(), matrix.size() * sizeof(rerun::Position3D));
+        return Collection<rerun::Position3D>::take_ownership(std::move(positions));
+    }
+};
+
+
 
 int main(int argc, char* argv[]) {
     string videoFilePath = "";
@@ -24,6 +113,18 @@ int main(int argc, char* argv[]) {
     else {
         videoFilePath = argv[1];
     }
+
+    // Create a new `RecordingStream` which sends data over TCP to the viewer process.
+    const auto rec = rerun::RecordingStream("rerun_example_cpp");
+    // Try to spawn a new viewer instance.
+    rec.spawn().exit_on_failure();
+
+    // Create some data using the `grid` utility function.
+    std::vector<rerun::Position3D> points = grid3d<rerun::Position3D, float>(-10.f, 10.f, 10);
+    std::vector<rerun::Color> colors = grid3d<rerun::Color, uint8_t>(0, 255, 10);
+
+    // Log the "my_points" entity with our data, using the `Points3D` archetype.
+    rec.log("my_points", rerun::Points3D(points).with_colors(colors).with_radii({ 0.5f }));
 
     VideoCapture capture(videoFilePath);
     if (!capture.isOpened()) {
